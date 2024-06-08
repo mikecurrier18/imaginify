925256e5b939970168ca036fd32f261586d447a8
Michael Currier <mikecurrier18@gmail.com>
2024-06-08 17:08:02 -0400

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>home</p>
+      {/* <UserButton afterSignOutUrl='/'/> */}
+    </div>
+  )
+}
+
+export default Home
