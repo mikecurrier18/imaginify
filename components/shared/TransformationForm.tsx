0b07a5ef0caec9329e3013bf091d1cc284162872
Michael Currier <mikecurrier18@gmail.com>
2024-06-10 22:28:31 -0400

    began front-end components, fixed db connection issue

---
+++ components/shared/TransformationForm.tsx
@@ -0,0 +1,214 @@
+"use client";
+
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  aspectRatioOptions,
+  defaultValues,
+  transformationTypes,
+} from "@/constants";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { CustomField } from "./CustomField";
+import { AspectRatioKey, debounce } from "@/lib/utils";
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+});
+
+const TransformationForm = ({
+  action,
+  data = null,
+  userId,
+  type,
+  creditBalance,
+}: TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+  const [image, setImage] = useState(data);
+  const [transformation, setTransformation] = useState<Transformations | null>(
+    null
+  );
+  const [submitting, setSubmitting] = useState(false);
+  const [transforming, setTransforming] = useState(false);
+  const [transformationConfig, setTransformationConfig] = useState(false);
+  const initialValues =
+    data && action === "Update"
+      ? {
+          title: data?.title,
+          publicId: data?.publicId,
+          aspectRatio: data?.aspectRatio,
+          color: data?.color,
+          prompt: data?.prompt,
+        }
+      : defaultValues;
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  const onSelectFieldHandler = (
+    value: string,
+    onChangeField: (value: string) => void
+  ) => {
+    const imageSize = aspectRatioOptions[value as AspectRatioKey]
+    setImage((prevState: any) => ({
+        ...prevState,
+        aspectRatio: imageSize.aspectRatio,
+        width: imageSize.width,
+        height: imageSize.height,
+    }))
+    setTransformation(transformationType.config);
+    return onChangeField(value);
+  };
+
+  const onInputChangeHandler = (
+    fieldName: string,
+    value: string,
+    type: string,
+    onChangeField: (value: string) => void
+  ) => {
+    debounce(()=>{},1000);
+  };
+
+  const onTransformHandler = () => {};
+  return (
+    <div>
+      {" "}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <CustomField
+            control={form.control}
+            name="title"
+            formLabel="Image Title"
+            className="w-full"
+            render={({ field }) => <Input {...field} className="input-field" />}
+          />
+
+          {type === "fill" && (
+            <CustomField
+              control={form.control}
+              name="aspectRatio"
+              formLabel="Aspect Ratio"
+              className="w-full"
+              render={({ field }) => (
+                <Select
+                  onValueChange={(value) =>
+                    onSelectFieldHandler(value, field.onChange)
+                  }
+                >
+                  <SelectTrigger className="select-field">
+                    <SelectValue placeholder="Select size" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {Object.keys(aspectRatioOptions).map((key) => (
+                      <SelectItem key={key} value={key} className="select-item">
+                        {aspectRatioOptions[key as AspectRatioKey].label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              )}
+            />
+          )}
+          {(type === "remove" || type === "recolor") && (
+            <div className="prompt-field">
+              <CustomField
+                control={form.control}
+                name="prompt"
+                formLabel={
+                  type === "remove" ? "Object to remove" : "Object to recolor"
+                }
+                className="w-full"
+                render={({ field }) => (
+                  <Input
+                    value={field.value}
+                    className="input-field"
+                    onChange={(e) =>
+                      onInputChangeHandler(
+                        "prompt",
+                        e.target.value,
+                        type,
+                        field.onChange
+                      )
+                    }
+                  />
+                )}
+              />
+
+              {type === "recolor" && (
+                <CustomField
+                  control={form.control}
+                  name="color"
+                  formLabel="Replacement color"
+                  className="w-full"
+                  render={({ field }) => (
+                    <Input
+                      value={field.value}
+                      className="input-field"
+                      onChange={(e) =>
+                        onInputChangeHandler(
+                          "color",
+                          e.target.value,
+                          "recolor",
+                          field.onChange
+                        )
+                      }
+                    />
+                  )}
+                />
+              )}
+            </div>
+          )}
+          <div className="flex flex-col gap-4">
+            <Button
+              className="submit-button capitalize"
+              disabled={transforming || transformation === null}
+              onClick={onTransformHandler}
+              type="button"
+            >
+              {transforming ? "Transforming..." : "Apply Transformation"}
+            </Button>
+            <Button
+              className="submit-button capitalize"
+              disabled={submitting}
+              type="submit"
+            >
+              {submitting ? "Submitting..." : "Save Image"}
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default TransformationForm;
